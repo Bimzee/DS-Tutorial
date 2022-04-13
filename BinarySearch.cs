7fd31e2551b81185f4606c61b1fc2cea8d75675c
Varghese, Bimal <bimal.varghese@boeing.com>
2022-04-13 08:48:41 +0530

    Implemented IsAnagram

---
+++ BinarySearch.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructure
+{
+    public class BinarySearch
+    {
+        public class Node
+        {
+            public int data;
+            public Node left;
+            public Node right;
+
+            public void Display()
+            {
+                Console.Write(data + " ");
+            }
+        }
+        public Node root;
+
+        public BinarySearch()
+        {
+            root = null;
+        }
+
+        public Node ReturnRoot()
+        {
+            return root;
+        }
+        public void Preorder(Node Root)
+        {
+            //https://www.csharpstar.com/csharp-program-to-implement-binary-search-tree-traversal/
+            if (Root != null)
+            {
+                Console.Write(Root.data + " ");
+                Preorder(Root.left);
+                Preorder(Root.right);
+            }
+        }
+
+        public void Insert(int i)
+        {
+            Node newNode = new Node();
+
+            newNode.data = i;
+
+            if (root == null)
+                root = newNode;
+            else
+            {
+                Node current = root;
+                Node parent;
+                while (true)
+                {
+                    parent = current;
+                    if (i < current.data)
+                    {
+                        current = current.left;
+                        if (current == null)
+                        {
+                            parent.left = newNode;
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        current = current.right;
+                        if (current == null)
+                        {
+                            parent.right = newNode;
+                            break;
+                        }
+                    }
+                }
+            }
+
+        }
+    }
+}
---
+++ DuplicateCharRemoval.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataStructure
+{
+    public class DuplicateCharRemoval
+    {
+        public string RemoveDuplicate(string key)
+        {
+            //StringBuilder result = new StringBuilder();
+            StringBuilder table = new StringBuilder();
+
+            foreach (var item in key)
+            {
+                if(table.ToString().IndexOf(item) == -1)
+                {
+                    table.Append(item);
+                    //result.Append(item);
+                }
+            }
+
+
+
+            return table.ToString();
+        }
+
+        public bool IsAnagram(string key1, string key2)
+        {
+            bool IsAnagram= false;
+            var input1 = key1.ToLower().ToCharArray();
+            var input2 = key2.ToLower().ToCharArray();
+
+            if(input1.Length == input2.Length)
+            {
+                var output = input1.Intersect(input2);
+                IsAnagram = output.Count() == input1.Length;
+            }
+
+            return IsAnagram;
+        }
+    }
+}
