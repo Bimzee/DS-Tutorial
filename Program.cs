5210d61b092cd9be36c04c061b1e586ab926d856
Varghese, Bimal <bimal.varghese@boeing.com>
2022-04-13 19:20:01 +0530

    test

--- Program.cs
+++ Program.cs
@@ -7,6 +7,20 @@ namespace DataStructure
     {
         static void Main(string[] args)
         {
+            //var strDup = new string[] { "Csharpstar", "Google", "Yahoo", "CNN" };
+            //foreach (var item in strDup)
+            //{
+            //    CallRemoveDuplicate(item);
+            //}
+
+            var anagramString = new[] { ("Silent", "Listen"), ("post", "opts"), ("abc","acb"),("abb","aab") };
+            foreach (var item in anagramString)
+            {
+                var res = new DuplicateCharRemoval().IsAnagram(item.Item1, item.Item2);
+                Console.WriteLine($"IsAnagram {res}"); 
+            }
+
+
             //var n = new SimpleLikedList();
             //n.CreateAndPrint3Nodes();
 
@@ -23,6 +37,7 @@ namespace DataStructure
             n.Print();
             //LinkedListWithAdd<int>.Print(n);
             */
+            CallBinarySearch();
 
             int a = 1;
             int b = 2;
@@ -33,6 +48,31 @@ namespace DataStructure
 
         }
 
+        public static void CallBinarySearch()
+        {
+            BinarySearch nums = new BinarySearch();
+            nums.Insert(50);
+            nums.Insert(54);
+            nums.Insert(17);
+            nums.Insert(23);
+            nums.Insert(12);
+            nums.Insert(19);
+            nums.Insert(9);
+            nums.Insert(14);
+            nums.Insert(67);
+            nums.Insert(76);
+            nums.Insert(72);
+
+            nums.Preorder(nums.ReturnRoot());
+        }
+
+
+        public static void CallRemoveDuplicate(string key)
+        {
+
+            DuplicateCharRemoval duplicate = new DuplicateCharRemoval();
+            Console.WriteLine($"string after removing duplicate {duplicate.RemoveDuplicate(key)}");
+        }
        
 
     }
